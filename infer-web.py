efdecbca0fe78df17631e3015c1aaf40cda276bf
Xiao Mao <matthew99a@gmail.com>
2024-12-20 00:25:39 -0800

    Update infer-web.py

--- infer-web.py
+++ infer-web.py
@@ -742,7 +742,7 @@ def train_index(exp_dir1, version19, cluster=True, extended=False):
     big_npy = np.concatenate(npys, 0)
     big_npy_idx = np.arange(big_npy.shape[0])
     np.random.shuffle(big_npy_idx)
-    big_npy = big_npy[big_npy_idx[:200000]]
+    big_npy = big_npy[big_npy_idx]
     if (
         (cluster and big_npy.shape[0] > 2e5)
         or (extended and big_npy.shape[0] > 1e4)
