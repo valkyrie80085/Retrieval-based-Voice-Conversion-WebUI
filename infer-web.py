f9169c004c8f5ac53c4381705ba769cba297bdf6
Xiao Mao <matthew99a@gmail.com>
2024-12-19 18:10:17 -0800

    Update infer-web.py

--- infer-web.py
+++ infer-web.py
@@ -714,7 +714,7 @@ def click_train(
 
 
 # but4.click(train_index, [exp_dir1], info3)
-def train_index(exp_dir1, version19, cluster=False, extended=False):
+def train_index(exp_dir1, version19, cluster=True, extended=False):
     # exp_dir = "%s/logs/%s" % (now_dir, exp_dir1)
     exp_dir = "logs/%s" % (exp_dir1)
     os.makedirs(exp_dir, exist_ok=True)
